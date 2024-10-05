0785c4205f6a1bdd407a96e05a4c77754a32f23d
Aloïs FOURNIER <aloisfournier.pro@gmail.com>
2024-10-05 17:37:25 +0200

    initial prototype

---
+++ src/app/components/NewsHeader.tsx
@@ -0,0 +1,11 @@
+
+export default function NewsHeader() {
+    return(
+        <>
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 bg-gray-300"
+            >
+                <p className="text-center">zenhosting - ouverture prochaine </p>
+            </div>
+        </>
+    )
+}
